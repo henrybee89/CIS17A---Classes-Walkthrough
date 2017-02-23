9f48efcaf6baa3cf2573d722144edd5ad8e671bc
henrybee89 <henry89bee@gmail.com>
2017-02-22 20:20:52 -0800

    Added Course

---
+++ Course.cpp
@@ -0,0 +1,39 @@
+#include "Course.h"
+#include <string>
+using namespace std;
+
+Course::Course(string name, string description, Teacher teacher)
+	: _name(name), _description(description), _teacher(teacher)
+{
+}
+
+Course::~Course()
+{
+}
+
+bool Course::EnrollStudent(Student newStudent, int position)
+{
+	if (position <= 9 && _students[position].GetName() == "Null")
+	{
+		_students[position] = newStudent;
+		return true;
+	}
+	return false;
+}
+
+string Course::GetClassInfo()
+{
+	string info = "Course Information for " + _name + "\n";
+	info += _description + "\n";
+	info += "-------------------------------";
+	info += "\n";
+	for (auto student : _students)
+	{
+		if (student.GetName() != "Null")
+		{
+			info += student.GetName() + "\n";
+		}
+	}
+	return info;
+}
+
--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
 #include "Student.h"
 #include "Teacher.h"
 #include <iostream>
-
+#include "Course.h"
 using namespace std;
 
 int main()
 {
 	Teacher james = Teacher("james");
-	//Student bob = Student("bob");
+	Course cis17 = Course("CIS17A", "Programing C++: Objects", james);
+	Student bob = Student("bob");
+	Student jane = Student("jane");
+
+	cis17.EnrollStudent(bob, 0);
+	cis17.EnrollStudent(jane, 1);
+
+	cout << cis17.GetClassInfo() << endl;
 
 
 
